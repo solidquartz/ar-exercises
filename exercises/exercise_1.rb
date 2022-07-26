58e68a62a81a8a04ddbf315e5008759ddb4fefd1
Aky Mac Donald <akymacd@gmail.com>
2022-07-26 12:38:55 -0400

    Exercise 1 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,16 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+
+
+
+
+# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
+
+# - Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# - Richmond (annual_revenue of 1260000 carries women's apparel only)
+# - Gastown (annual_revenue of 190000 carries men's apparel only)
