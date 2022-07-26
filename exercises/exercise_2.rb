0acb174a0725dffd6c56a9024253873b9b3b28be
Aky Mac Donald <akymacd@gmail.com>
2022-07-26 12:48:46 -0400

    Complete exerise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,7 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.update(name: "Uniqlo")
 
 
 # 1. Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
